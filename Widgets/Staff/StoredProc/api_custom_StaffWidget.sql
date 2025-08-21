6d2030b2d9ec827c6ca21ffb221c6ebd00044a37
Traci Vincent <tvincent@burnthickory.com>
2025-08-21 15:42:18 -0400

    Added a StaffWidget_web grouped by Dept

--- Widgets/Staff/StoredProc/api_custom_StaffWidget.sql
+++ Widgets/Staff/StoredProc/api_custom_StaffWidget.sql
@@ -18,9 +18,10 @@ GO
 -- Chris Kehayias
 -- Updates:
 -- 4/17/2024		- Initial Commit
+-- T.Vincent 3/29/2025 Customize for BHBC
 -- =============================================
 ALTER PROCEDURE [dbo].[api_custom_StaffWidget] 
-	@DomainID int,
+	@DomainID int = 1,
 	@UserName nvarchar(75) = null
 AS
 BEGIN
@@ -30,20 +31,21 @@ BEGIN
 		C.Nickname
 		,C.First_Name
 		,C.Last_Name
-		,C.Email_Address
+		,CASE WHEN S.Show_Email = 1 THEN C.Email_Address ELSE '' END AS Email_Address
 		,C.Mobile_Phone
 		,C.Contact_GUID
 		,S.Facebook_URL
-		,S.Title
+		,S.x_URL
+		,S.Job_Title AS Title
 		,Image_GUID = COALESCE(SF.Unique_Name, CF.Unique_Name)
 		,File_URL = CONCAT('https://', D.External_Server_Name, '/ministryplatformapi/files/')
-	FROM Staff S
+	FROM Staff_Members S
 	INNER JOIN Contacts C ON C.Contact_ID = S.Contact_ID
 	INNER JOIN dp_Domains D ON D.Domain_ID = @DomainID
 	LEFT OUTER JOIN dp_Files CF ON CF.Table_Name='Contacts' AND CF.Record_ID=C.Contact_ID AND CF.Default_Image = 1
-	LEFT OUTER JOIN dp_Files SF ON SF.Table_Name='Staff' AND SF.Record_ID=S.Staff_ID AND SF.Default_Image = 1
-	WHERE S.Start_Date < GETDATE()
-	AND (S.End_Date IS NULL OR S.End_Date > GETDATE())
+	LEFT OUTER JOIN dp_Files SF ON SF.Table_Name='Staff_Members' AND SF.Record_ID=S.Staff_Member_ID AND SF.Default_Image = 1
+	WHERE S.Hire_Date < GETDATE()
+	AND (S.Termination_Date IS NULL OR S.Termination_Date > GETDATE())
 	AND S.Show_Online = 1
 	ORDER BY S.Online_Order
 
